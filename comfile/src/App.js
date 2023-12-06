9dd2797f4c7a26019a08f41651585be8a1ea8edd
kimwoojin <kimwoojin1903@gmail.com>
2023-12-07 00:53:34 +0900

    react router를 통한 해쉬변경

--- comfile/src/App.js
+++ comfile/src/App.js
@@ -1,39 +1,70 @@
 import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
 
 const headstateData = [
-  {hash: "#home", text: "Home", name: "Home"},
-  {hash: "#team", text: "Team", name: "Team"},
-  {hash: "#member", text: "Member", name: "Member"},
-  {hash: "#purpose", text: "Purpose", name: "Purpose"},
+  { hash: "home", text: "Home", component: Home },
+  { hash: "team", text: "Team", component: Team },
+  { hash: "member", text: "Member", component: Member },
+  { hash: "purpose", text: "Purpose", component: Purpose },
 ];
+
+
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function Team() {
+  return <p>Team</p>;
+}
+
+function Member() {
+  return <p>Member</p>;
+}
+
+function Purpose() {
+  return <p>Purpose</p>;
+}
 
 function Head() {
   return (
     <div className="head">
       <ul>
-        <li>
-          <a href={headstateData[0].hash}>home</a>
-        </li>
-        <li>
-          <a href={headstateData[1].hash}>team</a>
-        </li>
-        <li>
-          <a href={headstateData[2].hash}>member</a>
-        </li>
-        <li>
-          <a href={headstateData[3].hash}>purpose</a>
-        </li>
+      {headstateData.map((item) => (
+          <li key={item.hash}>
+            <NavLink to={item.hash}>{item.text}</NavLink>
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
 
+
 function Board() {
   return (
     <div className="board">
-      <p>작성예정</p>
+      <Routes>
+        {headstateData.map((item) => (
+          <Route key={item.hash} path={`/${item.hash}`} element={<item.component />} />
+        ))}
+      </Routes>
     </div>
   );
 }
 
-export {Head, Board};
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Head />
+        <Board />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- comfile/src/index.js
+++ comfile/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {Head, Board} from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Head />
-    <Board />
+    <App />
   </React.StrictMode>
 );
 
