aeb01aded058321dbf65bf4d8bbffa8bf48471ad
kimwoojin <kimwoojin1903@gmail.com>
2023-12-08 00:20:13 +0900

    주석처리 완료

--- comfile/src/App.js
+++ comfile/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React, {useState, useEffect} from "react";
 import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
-import createMemo from "./createMemo.js";
 import {saveMemo} from "./saveMemo.js";
 import {loadMemo} from "./loadMemo.js";
 
@@ -13,17 +12,20 @@ const headstateData = [
 ];
 
 function Home() {
+  //useState훅을 이용해 상태를 변경할수 있는 함수를 선언하고 초기값 지정
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [memos, setMemos] = useState([]);
 
+  // useEffect훅을 이용해 컴포넌트가 렌더링 될 때마다 실행할 콜백을 등록
   useEffect(() => {
     const loadedMemos = loadMemo(); // loadMemo 함수를 사용하여 메모 데이터 로드
     setMemos(loadedMemos); // 불러온 메모 데이터를 상태로 설정
-  }, []);
+  }, []); // 의존성 배열을 빈 배열을 설정해 처음 마운트될때만 실행되게 함
+
 
   const handleSave = () => {
-    if (!title.trim() || !content.trim()) {
+    if (!title.trim() || !content.trim()) { // 입력창이 빈 문자열일시 경고창
       alert("이름과 내용을 입력하세요.");
       return;
     }
@@ -36,10 +38,10 @@ function Home() {
     };
 
     saveMemo(newMemo); // 메모 저장
-    setMemos((prevMemos) => [...prevMemos, newMemo]);
+    setMemos((prevMemos) => [...prevMemos, newMemo]); // 함수형 업데이트를 통해 memos에 newMemo를 추가한 새 배열 반환
 
     setTitle("");
-    setContent("");
+    setContent(""); // 입력창 초기화
   };
 
   const handleClear = () => {
@@ -52,7 +54,8 @@ function Home() {
       <div>DYABYA</div>
       <section className="memo-list">
         {memos.map((memo, index) => (
-          <div key={index}>
+          // react는 반복되는 요소를 생성할때 식별값을 줘야함
+          <div key={index}> 
             <h2>{memo.title}</h2>
             <p>{memo.content}</p>
             <p>{memo.timestamp}</p>
@@ -66,6 +69,7 @@ function Home() {
             className="title"
             placeholder="이름"
             value={title}
+            // onchange를 통해 입력창 값이 변경될때마다 title 상태를 업데이트
             onChange={(e) => setTitle(e.target.value)}
           />
           <textarea
