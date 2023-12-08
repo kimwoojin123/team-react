e5489346b08a045f1706fe409d9c99334a5f9d68
kimwoojin123 <kimwoojin1903@gmail.com>
2023-12-08 11:43:49 +0900

    memberPage작업, css작업

--- comfile/src/App.js
+++ comfile/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
 import {saveMemo} from "./saveMemo.js";
 import {loadMemo} from "./loadMemo.js";
@@ -11,11 +11,35 @@ const headstateData = [
   {hash: "purpose", text: "Purpose", component: Purpose},
 ];
 
+const memberData = {
+  kim: {
+    url: "https://github.com/kimwoojin123/",
+    hobby: ["음악감상", "자전거 타기"],
+    resolution: "팀원들의 스트레스를 줄이겠습니다!",
+  },
+  bang: {
+    url: "https://github.com/seunghee1108/",
+    hobby: ["음악감상", "산책"],
+    resolution: "빡세게 열심히 하겠습니다!",
+  },
+  jung: {
+    url: "https://github.com/JungYoungSick",
+    hobby: ["독서", "영화감상", "클래식감상", "자전거타기"],
+    resolution: "맡은 바 최선을 다 하겠습니다!",
+  },
+  choi: {
+    url: "https://github.com/csm009177",
+    hobby: ["컴퓨터 하드웨어 조립"],
+    resolution: "재밌게 같이 하겠습니다!",
+  },
+};
+
 function Home() {
   //useState훅을 이용해 상태를 변경할수 있는 함수를 선언하고 초기값 지정
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [memos, setMemos] = useState([]);
+  const memoListRef = useRef(null);
 
   // useEffect훅을 이용해 컴포넌트가 렌더링 될 때마다 실행할 콜백을 등록
   useEffect(() => {
@@ -23,9 +47,16 @@ function Home() {
     setMemos(loadedMemos); // 불러온 메모 데이터를 상태로 설정
   }, []); // 의존성 배열을 빈 배열을 설정해 처음 마운트될때만 실행되게 함
 
+  useEffect(() => {
+    const container = memoListRef.current;
+    if (container) {
+      container.scrollTop = container.scrollHeight;
+    }
+  }, [memos]);
 
   const handleSave = () => {
-    if (!title.trim() || !content.trim()) { // 입력창이 빈 문자열일시 경고창
+    if (!title.trim() || !content.trim()) {
+      // 입력창이 빈 문자열일시 경고창
       alert("이름과 내용을 입력하세요.");
       return;
     }
@@ -40,7 +71,6 @@ function Home() {
     saveMemo(newMemo); // 메모 저장
     setMemos((prevMemos) => [...prevMemos, newMemo]); // 함수형 업데이트를 통해 memos에 newMemo를 추가한 새 배열 반환
 
-    setTitle("");
     setContent(""); // 입력창 초기화
   };
 
@@ -52,10 +82,10 @@ function Home() {
   return (
     <div className="prompt">
       <div>DYABYA</div>
-      <section className="memo-list">
+      <section className="memo-list" ref={memoListRef}>
         {memos.map((memo, index) => (
           // react는 반복되는 요소를 생성할때 식별값을 줘야함
-          <div key={index}> 
+          <div key={index}>
             <h2>{memo.title}</h2>
             <p>{memo.content}</p>
             <p>{memo.timestamp}</p>
@@ -63,7 +93,7 @@ function Home() {
         ))}
       </section>
       <section className="container">
-        <form className="memo-Form" onSubmit={(e) => e.preventDefault()}>
+        <form className="memo-form" onSubmit={(e) => e.preventDefault()}>
           <input
             type="text"
             className="title"
@@ -95,9 +125,33 @@ function Team() {
 }
 
 function Member() {
-  return <p>Member</p>;
-}
+  const [members, setMembers] = useState(null);
 
+  const clickMember = (member) => {
+    setMembers(memberData[member]);
+  };
+
+  return (
+    <div className="memberContainer">
+      <div className="side">
+        {Object.keys(memberData).map((member) => (
+          <li key={member} className={member} onClick={() => clickMember(member)}>
+            {member}
+          </li>
+        ))}
+      </div>
+      <div className="memberBoard">
+        {members && (
+          <div>
+            <p>{members.url}</p>
+            <p>Hobbies: {members.hobby.join(", ")}</p>
+            <p>Resolution: {members.resolution}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 function Purpose() {
   return <p>Purpose</p>;
 }
